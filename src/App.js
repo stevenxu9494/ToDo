424467962989cc63f75235de34de7995308609f2
Steven Xu <stevenxu9494@gmail.com>
2020-06-16 13:40:13 +1000

    Add To-Do Items

--- src/App.js
+++ src/App.js
@@ -7,18 +7,50 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      userName: "Steven"
+      userName: "Steven",
+      todoItems: [{ action: "Buy Flowers", done: false },
+                  { action: "Get Shoes", done: false },
+                  { action: "Collect Tickets", done: true },
+                  { action: "Call Joe", done: false }],
+      newItemText: ""
     }
   }
-  render() {
-    return (
+
+  updateNewTextValue = (event) => {
+    this.setState({ newItemText: event.target.value });
+  }
+
+  createNewTodo = () => {
+    if (!this.state.todoItems
+      .find(item => item.action === this.state.newItemText)) {
+        this.setState({
+          todoItems: [...this.state.todoItems,
+            { action: this.state.newItemText, done: false }],
+          newItemText: ""
+        });
+    }
+  }
+
+  // render() {
+  //   return (
+  render = () =>
       <div>
         <h4 className="bg-primary text-white text-center p-2">
           { this.state.userName}'s To Do List
+          ({ this.state.todoItems.filter(item => !item.done).length} items to do)
         </h4>
+        <div className="container-fluid">
+          <div className="my-1">
+            <input className="form-control"
+              value={ this.state.newItemText }
+              onChange={ this.updateNewTextValue } />
+            <button className="btn btn-primary mt-1" 
+              onClick={ this.createNewTodo }>Add</button>
+          </div>
+        </div>
       </div>
-    )
-  };
+  //   )
+  // };
 }
 
 // export default App;
